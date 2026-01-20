7c9dd18e03d9bca3677d7e42294b7d43466d9f3d
gokul <gokul.sridharan@blackcatsolutions.co.uk>
2026-01-20 11:30:14 +0000

    tiny spacing update

--- petstore-consumer/src/test/java/OrderId.java
+++ petstore-consumer/src/test/java/OrderId.java
@@ -1,5 +1,4 @@
 public class OrderId {
-
     public static final String EXISTING_ORDER_ID = "12345";
     public static final String NON_EXISTING_ORDER_ID = "00000";
     public static final String INVALID_ORDER_ID = "abcde";
